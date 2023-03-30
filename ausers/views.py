e221e10854c681ddb3135f137baed1b01dbcecc0
shamspias <shamsuddinpias0@gmail.com>
2023-03-30 12:47:58 +0600

    fix: count

--- ausers/views.py
+++ ausers/views.py
@@ -111,7 +111,6 @@ class TrackConversationHistory(views.APIView):
         number_obj, created = NoneExistNumbers.objects.get_or_create(number=number)
         if created:
             number_obj.is_user = True
-            number_obj.text_count += 1
             number_obj.save()
         user_input = request.data.get('user_input')
         end_param = request.data.get('end_param', 'bot')
