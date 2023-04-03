f2fc52cd37dc854bb5d882f183b61755adfe2600
shamspias <shamsuddinpias0@gmail.com>
2023-04-03 16:28:23 +0600

    fix: search admin

--- ausers/admin.py
+++ ausers/admin.py
@@ -27,7 +27,7 @@ class UserAdmin(UserAdmin):
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
-    search_fields = ('email', 'stripe_id', 'phone_number')
+    search_fields = ('email', 'stripe_id', 'phone_number', 'first_name')
     list_display = ("email", "first_name", "phone_number", "stripe_id", "subscription_status")
 
 
@@ -37,6 +37,7 @@ class NoneExistNumberAdmin(admin.ModelAdmin):
     """
 
     list_display = ("number", "is_user", "text_count")
+    search_fields = ('number',)
 
 
 admin.site.register(NoneExistNumbers, NoneExistNumberAdmin)
